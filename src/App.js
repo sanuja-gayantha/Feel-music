76055ac2d3857701b0996e86d477e1b3fcfbfdb5
sanuja gayantha <sanuja@pop-os.localdomain>
2022-01-23 18:17:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // custom imports
 import Home from './pages/Home';
 import Error from './components/Error';
+import Updating from '/components/Updating';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home/>
+          <Updating/>
         </Route>
         <Route path='*'>
           <Error />
---
+++ src/components/Updating.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Updating = () => {
+    return(
+                <div className="update-center">
+                    <h1>Page is under developing!</h1>              
+                </div>
+    )
+}
+
+export default Updating;
